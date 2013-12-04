6b9f3cbd1ddf9622c1fffb0ece41bf28e0361dcf
Tom Curran <tom@tomcurran.org>
2013-12-04 20:55:13 +0000

    loader that loads and adapts files

---
+++ Sequential/src/Loader.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import model.AddTimerAdapter;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+public class Loader extends ClassLoader {
+
+	private static final String BIN_ADAPT = "bin-adapt";
+
+	@Override
+	protected Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
+		if (name.startsWith("java.")) {
+			return super.loadClass(name, resolve);
+		}
+
+		// gets an input stream to read the bytecode of the class
+		String resource = name.replace('.', '/') + ".class";
+		InputStream is = getResourceAsStream(resource);
+		byte[] b;
+
+		// adapts the class on the fly
+		try {
+			ClassReader cr = new ClassReader(is);
+			ClassWriter cw = new ClassWriter(0);
+			ClassVisitor cv = new AddTimerAdapter(cw);
+			cr.accept(cv, 0);
+			b = cw.toByteArray();
+		} catch (Exception e) {
+			throw new ClassNotFoundException(name, e);
+		}
+
+		// optional: stores the adapted class on disk
+		try {
+			File f = new File(Loader.BIN_ADAPT + File.separator + resource);
+			f.getParentFile().mkdirs();
+			FileOutputStream fos = new FileOutputStream(f);
+			fos.write(b);
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// returns the adapted class
+		return defineClass(name, b, 0, b.length);
+	}
+
+	public static void main(final String args[]) throws Exception {
+		// loads the application class (in args[0]) with an Adapt class loader
+		ClassLoader loader = new Loader();
+		Class<?> c = loader.loadClass(args[0]);
+		// calls the 'main' static method of this class with the
+		// application arguments (in args[1] ... args[n]) as parameter
+		Method m = c.getMethod("main", new Class<?>[] { String[].class });
+		String[] applicationArgs = new String[args.length - 1];
+		System.arraycopy(args, 1, applicationArgs, 0, applicationArgs.length);
+		m.invoke(null, new Object[] { applicationArgs });
+	}
+}
