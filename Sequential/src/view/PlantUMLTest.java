c66d3bf5af426af3129bdbd601871819b1cacb01
Tim Blackwell <timmblackwell@gmail.com>
2013-12-04 14:41:52 +0000

    creates dir if doesnt exist and first arg is file name

--- Sequential/src/view/PlantUMLTest.java
+++ Sequential/src/view/PlantUMLTest.java
@@ -13,18 +13,26 @@ import net.sourceforge.plantuml.SourceStringReader;
 public class PlantUMLTest {
 
 	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			System.out.println("Missing arguments, please provide output file name");
+			return;
+		}
+		
+		final String fileName = args[0] + ".svg";	
+		final String folder = "output";
+		
 		String source = Converter.toPlantUML(TestData.get());
 				
-//				
-
 		SourceStringReader reader = new SourceStringReader(source);
 		final ByteArrayOutputStream os = new ByteArrayOutputStream();
 		reader.generateImage(os, new FileFormatOption(FileFormat.SVG));
 		os.close();
 
 		final String svg = new String(os.toByteArray());
-		
-		PrintWriter writer = new PrintWriter("tmp"+ File.separator +"test.svg", "UTF-8");
+
+		File outputFile = new File(folder);
+		outputFile.mkdirs();
+		PrintWriter writer = new PrintWriter(folder + File.separator + fileName, "UTF-8");
 		writer.println(svg);
 		writer.close();
 	}
