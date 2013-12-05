d2cbeea4398d3c726724495572f3063a046a9784
Tim Blackwell <timmblackwell@gmail.com>
2013-12-05 20:43:54 +0000

    cleaned up

--- Sequential/src/main/Main.java
+++ Sequential/src/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import io.ClassReader;
 
 import java.io.File;
-import java.io.IOException;
-
 import asm.Loader;
 
 public class Main {
@@ -14,22 +12,16 @@ public class Main {
 		String outputPath = args[1];
 		String workingDir = "temp";
 		String interFile =  workingDir + File.separator + "inter.txt";
-		
+ 		String[] applicationArgs = new String[args.length - 2];
+ 		System.arraycopy(args, 1, applicationArgs, 0, applicationArgs.length);	
+ 		
 		try {
 			ClassReader.UnpackJar(pathToJar, workingDir);
 			String mainClassName = ClassReader.GetMainClassName(workingDir);
-			// call toms asm code with workdir and mainclassname
-	 		String[] applicationArgs = new String[args.length - 2];
-	 		System.arraycopy(args, 1, applicationArgs, 0, applicationArgs.length);
-	 		
 			Loader.RunAnalysis(mainClassName, interFile, applicationArgs);
 			io.Output.WriteSVG(interFile, outputPath);
 			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 				
